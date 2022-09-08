f1c284d67781d8464fef5416215bb0247cc008c1
Emmanuel Ibikunle <ibk.com@gmail.com>
2022-09-08 14:19:51 -0700

    8-write()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	int s = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	int s = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 }
