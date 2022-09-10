fb627725b280f5c6d47514c3dba135b6708dd8b7
Emmanuel Ibikunle <ibk.com@gmail.com>
2022-09-09 17:19:15 -0700

    7-print alphas

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
