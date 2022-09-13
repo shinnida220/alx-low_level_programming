feb0e1483379eeb8c2654d1ca490576c92291215
Emmanuel Ibikunle <ibk.com@gmail.com>
2022-09-13 13:44:28 -0700

    putchar- alphabett

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Uses _putchar to print alphabets
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
