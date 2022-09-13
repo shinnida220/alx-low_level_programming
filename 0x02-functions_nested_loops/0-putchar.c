58718f4cb9ec4e65051a6fe4c7ad508c35f34d06
Emmanuel Ibikunle <ibk.com@gmail.com>
2022-09-13 13:17:25 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar witha a new line
+ * return: 0
+ */
+int main(void)
+{
+	char textToPrint[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(textToPrint[i]);
+	}
+	_putchar('\n');
+
+	return (0);	
+}
