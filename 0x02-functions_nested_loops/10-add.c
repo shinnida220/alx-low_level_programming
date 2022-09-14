de388abfff26b3e5aeeb9586073f9800e84d4cd5
Nuel <ibk.com@gmail.com>
2022-09-14 01:43:24 +0100

    removed put_char

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,12 @@
 
 /**
  * add - adds two integers and returns the result.
+ * @num1: int
+ * @num2: int
  *
  * Return: int the result of addition
  */
 void add(int num1, int num2)
 {
-    return (num1 + num2);
+	return (num1 + num2);
 }
