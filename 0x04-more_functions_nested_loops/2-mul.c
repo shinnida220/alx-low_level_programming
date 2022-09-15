51b52c910a81675b01ea7e0f85fe88cf136cf096
Nuel <ibk.com@gmail.com>
2022-09-15 21:19:25 +0100

    task 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 /**
- * mull - a function that checks for a digit.
+ * mul - a function that checks for a digit.
  * @a: integer param
  * @b: integer param
  *
  * Return: int 1 or 0
  */
-int mull(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
