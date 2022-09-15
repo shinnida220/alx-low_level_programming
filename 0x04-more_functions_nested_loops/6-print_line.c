e4a48f799dd1a2fdb6b7c025d3520674401db342
Nuel <ibk.com@gmail.com>
2022-09-15 22:17:17 +0100

    task 6 - lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,10 +8,13 @@ void print_line(int n)
 {
     int i = 0;
 
-    while (i < n)
+    if (n > 0)
     {
-        _putchar('_');
-        ++i;
+        while (i < n)
+        {
+            _putchar('_');
+            ++i;
+        }
     }
     _putchar('\n');
 }
