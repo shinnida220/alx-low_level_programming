e7a0eab77ac710475dc19d54b32c4ea13138f043
Nuel <ibk.com@gmail.com>
2022-09-20 00:56:23 +0100

    pointers assignments array fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,12 +15,8 @@ void print_array(int *a, int n)
 	for (; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		/**_putchar(a[i]);*/
 		if (i != (n - 1))
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+			printf(", ");
 	}
-	_putchar('\n');
+	printf("\n");
 }
