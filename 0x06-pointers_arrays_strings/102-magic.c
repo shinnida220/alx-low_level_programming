f11138866601b71f668901239615ebae9afe0666
Nuel <ibk.com@gmail.com>
2022-09-22 18:21:50 +0100

    tab spacing

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - Magic
- *
- * Return: int success
- */
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
 
-	a[2] = 1024;
+	a[2] = 98;
 	p = &n;
 	/*
 	 * write your line of code here...
@@ -21,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	p[5] = 98;
+	;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
