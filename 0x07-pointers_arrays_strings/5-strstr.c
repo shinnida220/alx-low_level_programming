213add73a38e1055d8df2048f0fd07aec8aa1fd0
Nuel <ibk.com@gmail.com>
2022-09-26 20:57:50 +0100

    strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+/**
+ * _strstr - a function that locates a substring.
+ * @haystack: pointer to main string
+ * @needle: pointer to string to look for
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
