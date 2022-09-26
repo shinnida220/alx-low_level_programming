7608c3e3c57ebac8d5f67f2baa5315026b03459a
Nuel <ibk.com@gmail.com>
2022-09-26 20:28:21 +0100

    2-memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area.
+ * @src: memory area to copy from
+ * @dest: memory area to copy to
+ * @n: first n bytes of the memory area
+ * Return: *s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _main_H_
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 void _putchar(char c);
 
 #endif
