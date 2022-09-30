52a94db14cd834f9c0f75a78b70015ef825a8583
Nuel <ibk.com@gmail.com>
2022-09-30 21:09:42 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its name
+ *
+ * @argc: Argument count
+ * @argv: Array of arguments
+ * Return: int 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+# ARGC & ARGV
+
+Argument Count usually contains the number of arguments passed to the program
+
+Argument Vector is a one dimensional array of paramters passed to the program, the 1st ine being the name of the program itself
