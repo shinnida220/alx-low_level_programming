9688f72f5a560adb72b669d2d11842cc9d181f05
Nuel <ibk.com@gmail.com>
2022-09-30 21:32:41 +0100

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 /**
- * main - pmultiplies two argument numbers.
+ * main - multiplies two argument numbers.
  * @argc: number of command line arguments.
  * @argv: array of command line arguments.
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	if (argc != 3)
+	{
 		printf("Error\n");
+		return (1);
+	}
 	else
+	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
+		return (0);
+	}
 }
