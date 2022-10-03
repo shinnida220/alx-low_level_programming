59700ce1cd035147f3c609f507fc3b79fa463533
Nuel <ibk.com@gmail.com>
2022-10-03 18:56:27 +0100

    reworked shell script

---
+++ pointers/3-arrays.c
@@ -0,0 +1,14 @@
+/**
+ * Arrays
+ * Arrays in C are contiguous memory areas
+ * that hold a number of values of the same type.
+ *
+ * Unlike some other languages, in C,
+ * all elements of an array have the same type.
+ *
+ * To declare an array we use this syntax:
+ * type var_name[number_of_elements];,
+ *
+ * where number_of_elements is the number of elements
+ * of type type that we need.
+ */
