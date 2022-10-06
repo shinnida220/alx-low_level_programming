df657d019a32f8d834870a20355a115ec9955f2b
Nuel <ibk.com@gmail.com>
2022-10-06 13:24:12 +0100

    fixed a couple errors

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,29 +9,29 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *tmp, *new_ptr;
-    unsigned int i;
+	char *clone, *relloc;
+	unsigned int i;
 
-    if (ptr == NULL)
-        return (malloc(new_size));
-
-    if (new_size == old_size)
-        return (ptr);
-
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (0);
-    }
-
-    new_ptr = malloc(new_size);
-    if (new_ptr == NULL)
-        return (0);
-
-    tmp = ptr;
-    for (i = 0; i < (old_size || i < new_size); i++)
-        *(new_ptr + i) = tmp[i];
-
-    free(ptr);
-    return (new_ptr);
+	if (ptr != NULL)
+		clone = ptr;
+	else
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+		return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
+	free(ptr);
+	return (relloc);
 }
