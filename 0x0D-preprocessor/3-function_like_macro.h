beff391ed21052aee35cf11bd6419db2aec3a084
Nuel <ibk.com@gmail.com>
2022-10-10 21:13:53 +0100

    working on macros

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _main_H_
+#define _main_H_
+
+#define ABS(x)       \
+	do {             \
+		if (x < 1)   \
+		{            \
+			x *= -1; \
+		}            \
+	} while (0)
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _main_H_
+#define _main_H_
+
+#define SUB(a, b) ((a) + (b))
+
+#endif
