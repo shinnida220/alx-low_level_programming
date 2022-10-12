99362c0a605c1b50a0348c3e2d0b8167b4a6b6d6
Nuel <ibk.com@gmail.com>
2022-10-13 00:16:20 +0100

    task 4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the product of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds the remainder of the division
+ * @a: first number
+ * @b: second number
+ * Return: remainder of division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
