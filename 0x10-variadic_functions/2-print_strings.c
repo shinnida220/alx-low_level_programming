073a7594625a2c9e769c28d336d6025dbeb49f3a
Nuel <ibk.com@gmail.com>
2022-10-13 22:51:45 +0100

    task 3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,15 +9,15 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list ap;
 	char *str;
 	unsigned int i;
 
-	va_start(valist, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(valist, char *);
+		str = va_arg(ap, char *);
 		if (str)
 			printf("%s", str);
 		else
@@ -28,5 +28,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 
 	printf("\n");
-	va_end(valist);
+	va_end(ap);
 }
