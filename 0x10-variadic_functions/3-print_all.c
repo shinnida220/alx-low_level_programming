38dc70f53b2e74a5281aac6db167bbbf807b24da
Nuel <ibk.com@gmail.com>
2022-10-13 23:03:41 +0100

    task 4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,7 +40,8 @@ void print_all(const char * const format, ...)
 				str = va_arg(ap, char *), c = 1;
 				if (!str)
 				{
-					printf("(nil)"), break;
+					printf("(nil)");
+					break;
 				}
 				printf("%s", str);
 				break;
