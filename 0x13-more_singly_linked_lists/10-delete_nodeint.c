ce447143175adce429d37ef82e573ac758ae1f46
Nuel <ibk.com@gmail.com>
2022-10-24 21:34:03 +0100

    believe i'm done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,38 +3,42 @@
  * delete_nodeint_at_index - deletes the node at index
  * of a linked list.
  * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
+ * @index: index of the deleted node
+ *
  * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *prev_node;
-	listint_t *next_node;
+	listint_t *prev;
+	listint_t *next;
 
-	prev_node = *head;
+	prev = *head;
 
 	if (index != 0)
 	{
-		for (i = 0; i < index - 1 && prev_node != NULL; i++)
-			prev_node = prev_node->next;
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
 	}
 
-	if ((prev_node->next == NULL && index != 0) || prev_node == NULL)
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
 		return (-1);
+	}
 
-	next_node = prev_node->next;
+	next = prev->next;
 
 	if (index != 0)
 	{
-		prev_node->next = next_node->next;
-		free(next_node);
+		prev->next = next->next;
+		free(next);
 	}
 	else
 	{
-		free(prev_node);
-		*head = next_node;
+		free(prev);
+		*head = next;
 	}
 
 	return (1);
